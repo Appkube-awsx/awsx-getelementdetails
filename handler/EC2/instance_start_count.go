46012858f13205803a813a57933a2df89e6616f4
mohit-sharma-synectiks <mohit.sharma@synectiks.com>
2024-02-29 18:56:51 +0530

    updated instance_start_count.go

--- handler/EC2/instance_start_count.go
+++ handler/EC2/instance_start_count.go
@@ -2,90 +2,80 @@ package EC2
 
 import (
 	"fmt"
-	"log"
-	"time"
-
-	"github.com/Appkube-awsx/awsx-common/authenticate"
 	"github.com/Appkube-awsx/awsx-common/awsclient"
+	"github.com/Appkube-awsx/awsx-common/model"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
-
-	"github.com/Appkube-awsx/awsx-common/model"
 	"github.com/spf13/cobra"
+	"log"
+	"time"
 )
 
-var AwsxEc2StartCountCmd = &cobra.Command{
-	Use:   "instance_start_count",
-	Short: "Get instance start count metric data",
-	Long:  `Command to get instance start count metric data`,
+func GetInstanceStartCountPanel(cmd *cobra.Command, clientAuth *model.Auth, cloudWatchLogs *cloudwatchlogs.CloudWatchLogs) {
+	logGroupName, _ := cmd.PersistentFlags().GetString("logGroupName")
+	filterPattern, _ := cmd.PersistentFlags().GetString("filterPattern")
+	startTimeStr, _ := cmd.PersistentFlags().GetString("startTime")
+	endTimeStr, _ := cmd.PersistentFlags().GetString("endTime")
 
-	Run: func(cmd *cobra.Command, args []string) {
-		var clientAuth *model.Auth
-		var jsonString bool
-		var err error
+	var startTime, endTime *time.Time
 
-		jsonString, clientAuth, err = authenticate.AuthenticateCommand(cmd)
+	// Parse start time if provided
+	if startTimeStr != "" {
+		parsedStartTime, err := time.Parse(time.RFC3339, startTimeStr)
 		if err != nil {
-			log.Printf("Error during authentication: %v\n", err)
+			log.Printf("Error parsing start time: %v", err)
 			err := cmd.Help()
 			if err != nil {
-				return
+				// handle error
 			}
-			return
 		}
-		fmt.Println(jsonString)
-		startTimeStr, _ := cmd.PersistentFlags().GetString("startTime")
-		endTimeStr, _ := cmd.PersistentFlags().GetString("endTime")
-
-		var startTime, endTime time.Time
-
-		if startTimeStr != "" {
-			startTime, err = time.Parse(time.RFC3339, startTimeStr)
-			if err != nil {
-				log.Printf("Error parsing start time: %v", err)
-				return
-			}
-		} else {
-			startTime = time.Now().AddDate(0, -12, 0) // 12 months ago
-		}
-
-		if endTimeStr != "" {
-			endTime, err = time.Parse(time.RFC3339, endTimeStr)
-			if err != nil {
-				log.Printf("Error parsing end time: %v", err)
-				return
-			}
-		} else {
-			endTime = time.Now() // Current time
-		}
-
-		err = GetInstanceStartCountMetricData(clientAuth, startTime, endTime)
-		if err != nil {
-			log.Println("Error getting instance start count metric data: ", err)
-			return
-		}
-	},
-}
-
-func GetInstanceStartCountMetricData(clientAuth *model.Auth, startTime, endTime time.Time) error {
-	cwLogs := awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH_LOG).(*cloudwatchlogs.CloudWatchLogs)
-
-	// Start the query
-	queryInput := &cloudwatchlogs.StartQueryInput{
-		LogGroupName: aws.String("CloudTrail/DefaultLogGroup"), // Specify the log group name
-		StartTime:    aws.Int64(startTime.Unix()),
-		EndTime:      aws.Int64(endTime.Unix()),
-
-		QueryString: aws.String(`fields @timestamp, @message
-            | filter eventSource=="ec2.amazonaws.com"
-            | filter eventName=="StartInstances"
-            | stats count(*) as InstanceCount by bin(1mo)
-            | sort @timestamp desc`), // Your CloudWatch Logs Insights query
+		startTime = &parsedStartTime
+	} else {
+		defaultStartTime := time.Now().Add(-5 * time.Minute)
+		startTime = &defaultStartTime
 	}
 
-	queryResult, err := cwLogs.StartQuery(queryInput)
+	if endTimeStr != "" {
+		parsedEndTime, err := time.Parse(time.RFC3339, endTimeStr)
+		if err != nil {
+			log.Printf("Error parsing end time: %v", err)
+			err := cmd.Help()
+			if err != nil {
+				// handle error
+			}
+		}
+		endTime = &parsedEndTime
+	} else {
+		defaultEndTime := time.Now()
+		endTime = &defaultEndTime
+	}
+
+	events, err := filterCloudWatchLogs(clientAuth, startTime, endTime, logGroupName, filterPattern, cloudWatchLogs)
 	if err != nil {
-		return fmt.Errorf("failed to start query: %v", err)
+		log.Println("Error in getting sample count: ", err)
+		// handle error
+	}
+	for _, event := range events {
+		fmt.Println(event)
+	}
+}
+
+func filterCloudWatchLogs(clientAuth *model.Auth, startTime, endTime *time.Time, logGroupName string, filterPattern string, cloudWatchLogs *cloudwatchlogs.CloudWatchLogs) ([]*cloudwatchlogs.ResultField, error) {
+	// Construct input parameters
+	params := &cloudwatchlogs.StartQueryInput{
+		LogGroupName: aws.String(logGroupName),
+		StartTime:    aws.Int64(startTime.Unix() * 1000),
+		EndTime:      aws.Int64(endTime.Unix() * 1000),
+		QueryString:  aws.String(filterPattern),
+	}
+
+	if cloudWatchLogs == nil {
+		cloudWatchLogs = awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH_LOG).(*cloudwatchlogs.CloudWatchLogs)
+	}
+
+	queryResult, err := cloudWatchLogs.StartQuery(params)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start query: %v", err)
 	}
 
 	queryId := queryResult.QueryId
@@ -97,9 +87,9 @@ func GetInstanceStartCountMetricData(clientAuth *model.Auth, startTime, endTime 
 			QueryId: queryId,
 		}
 
-		queryResults, err = cwLogs.GetQueryResults(queryStatusInput) // Assign value to queryResults
+		queryResults, err = cloudWatchLogs.GetQueryResults(queryStatusInput) // Assign value to queryResults
 		if err != nil {
-			return fmt.Errorf("failed to get query results: %v", err)
+			return nil, fmt.Errorf("failed to get query results: %v", err)
 		}
 
 		queryStatus = aws.StringValue(queryResults.Status)
@@ -107,76 +97,12 @@ func GetInstanceStartCountMetricData(clientAuth *model.Auth, startTime, endTime 
 	}
 
 	// Query is complete, now process results
+	var results []*cloudwatchlogs.ResultField
 	for _, resultRow := range queryResults.Results {
 		for _, resultField := range resultRow {
-			fmt.Println(*resultField.Value)
+			results = append(results, resultField)
 		}
 	}
 
-	return nil
+	return results, nil
 }
-
-func init() {
-	AwsxEc2StartCountCmd.PersistentFlags().String("startTime", "", "start time (RFC3339 format)")
-	AwsxEc2StartCountCmd.PersistentFlags().String("endTime", "", "end time (RFC3339 format)")
-}
-
-// package EC2
-
-// import (
-//     "fmt"
-//     "github.com/Appkube-awsx/awsx-common/awsclient"
-//     "github.com/aws/aws-sdk-go/aws"
-//     "github.com/aws/aws-sdk-go/aws/awserr"
-//     "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
-//     "time"
-
-//     "github.com/Appkube-awsx/awsx-common/model"
-// )
-
-// func GetInstanceStartCountMetricData(clientAuth *model.Auth) error {
-//     cwLogs := awsclient.GetClient(*clientAuth, awsclient.CLOUD_WATCH_LOGS).(*cloudwatchlogs.CloudWatchLogs)
-
-//     // Start the query
-//     queryInput := &cloudwatchlogs.StartQueryInput{
-//         LogGroupName: aws.String("CloudTrail/DefaultLogGroup"), // Specify the log group name
-//         StartTime:    aws.Int64(time.Now().Add(-12 * 30 * 24 * time.Hour).Unix()), // Example start time (12 months ago)
-//         EndTime:      aws.Int64(time.Now().Unix()),                   // Example end time (current time)
-//         QueryString:  aws.String(`fields @timestamp, @message
-//             | filter eventSource=="ec2.amazonaws.com"
-//             | filter eventName=="StartInstances"
-//             | stats count(*) as InstanceCount by bin(1mo)
-//             | sort @timestamp desc`), // Your CloudWatch Logs Insights query
-//     }
-
-//     queryResult, err := cwLogs.StartQuery(queryInput)
-//     if err != nil {
-//         return fmt.Errorf("failed to start query: %v", err)
-//     }
-
-//     queryId := queryResult.QueryId
-//     queryStatus := ""
-//     for queryStatus != "Complete" {
-//         // Check query status
-//         queryStatusInput := &cloudwatchlogs.GetQueryResultsInput{
-//             QueryId: queryId,
-//         }
-
-//         queryResults, err := cwLogs.GetQueryResults(queryStatusInput)
-//         if err != nil {
-//             return fmt.Errorf("failed to get query results: %v", err)
-//         }
-
-//         queryStatus = aws.StringValue(queryResults.Status)
-//         time.Sleep(1 * time.Second) // Wait for a second before checking status again
-//     }
-
-//     // Query is complete, now process results
-//     for _, resultRow := range queryResults.Results {
-//         for _, resultField := range resultRow {
-//             fmt.Println(*resultField.Value)
-//         }
-//     }
-
-//     return nil
-// }
