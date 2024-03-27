7e25835cecb1d30bf28a0c04185abe2307bd5ae3
Syed Fayazuddin <syed.fayazuddin@synectiks.com>
2024-03-27 10:16:32 +0530

    updated throttles panel for timeseries data

--- handler/Lambda/throttles_panel.go
+++ handler/Lambda/throttles_panel.go
@@ -14,143 +14,133 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type ThrottleResult struct {
-    Value float64 `json:"Value"`
+type ThrottleData struct {
+	RawData []struct {
+		Timestamp time.Time
+		Value     float64
+	} `json:"RawData"`
 }
 
 var AwsxLambdaThrottleCmd = &cobra.Command{
-    Use:   "throttles_panel",
-    Short: "get throttle metrics data",
-    Long:  `command to get throttle metrics data`,
+	Use:   "throttle_panel",
+	Short: "get lambda throttle metrics data",
+	Long:  `Command to get lambda throttle metrics data`,
 
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("running from child command")
-        var authFlag, clientAuth, err = authenticate.AuthenticateCommand(cmd)
-        if err != nil {
-            log.Printf("Error during authentication: %v\n", err)
-            err := cmd.Help()
-            if err != nil {
-                return
-            }
-            return
-        }
-        if authFlag {
-            responseType, _ := cmd.PersistentFlags().GetString("responseType")
-            jsonResp, cloudwatchMetricResp, err := GetLambdaThrottleData(cmd, clientAuth, nil)
-            if err != nil {
-                log.Println("Error getting lambda throttle data : ", err)
-                return
-            }
-            if responseType == "frame" {
-                fmt.Println(cloudwatchMetricResp)
-            } else {
-                fmt.Println(jsonResp)
-            }
-        }
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Println("Running from child command")
+		authFlag, clientAuth, err := authenticate.AuthenticateCommand(cmd)
+		if err != nil {
+			log.Printf("Error during authentication: %v\n", err)
+			return
+		}
+		if !authFlag {
+			log.Println("Authentication failed")
+			return
+		}
 
-    },
+		responseType, _ := cmd.PersistentFlags().GetString("responseType")
+		if responseType != "json" && responseType != "frame" {
+			log.Println("Invalid response type. Valid options are 'json' or 'frame'.")
+			return
+		}
+
+		jsonResp, cloudwatchMetricResp, err := GetLambdaThrottleData(cmd, clientAuth, nil)
+		if err != nil {
+			log.Println("Error getting lambda throttle data: ", err)
+			return
+		}
+
+		if responseType == "frame" {
+			fmt.Println(cloudwatchMetricResp)
+		} else {
+			fmt.Println(jsonResp)
+		}
+	},
 }
 
-func GetLambdaThrottleData(cmd *cobra.Command, clientAuth *model.Auth, cloudWatchClient *cloudwatch.CloudWatch) (string, map[string]float64, error) {
-    startTimeStr, _ := cmd.PersistentFlags().GetString("startTime")
-    endTimeStr, _ := cmd.PersistentFlags().GetString("endTime")
+func GetLambdaThrottleData(cmd *cobra.Command, clientAuth *model.Auth, cloudWatchClient *cloudwatch.CloudWatch) (string, map[string]*cloudwatch.GetMetricDataOutput, error) {
+	startTimeStr, _ := cmd.PersistentFlags().GetString("startTime")
+	endTimeStr, _ := cmd.PersistentFlags().GetString("endTime")
 
-    var startTime, endTime *time.Time
+	var startTime, endTime *time.Time
 
-    // Parse start time if provided
-    if startTimeStr != "" {
-        parsedStartTime, err := time.Parse(time.RFC3339, startTimeStr)
-        if err != nil {
-            log.Printf("Error parsing start time: %v", err)
-            return "", nil, err
-        }
-        startTime = &parsedStartTime
-    } else {
-        defaultStartTime := time.Now().Add(-5 * time.Minute)
-        startTime = &defaultStartTime
-    }
+	// Parse start time if provided
+	if startTimeStr != "" {
+		parsedStartTime, err := time.Parse(time.RFC3339, startTimeStr)
+		if err != nil {
+			return "", nil, err
+		}
+		startTime = &parsedStartTime
+	} else {
+		defaultStartTime := time.Now().Add(-5 * time.Minute)
+		startTime = &defaultStartTime
+	}
 
-    if endTimeStr != "" {
-        parsedEndTime, err := time.Parse(time.RFC3339, endTimeStr)
-        if err != nil {
-            log.Printf("Error parsing end time: %v", err)
-            return "", nil, err
-        }
-        endTime = &parsedEndTime
-    } else {
-        defaultEndTime := time.Now()
-        endTime = &defaultEndTime
-    }
+	if endTimeStr != "" {
+		parsedEndTime, err := time.Parse(time.RFC3339, endTimeStr)
+		if err != nil {
+			return "", nil, err
+		}
+		endTime = &parsedEndTime
+	} else {
+		defaultEndTime := time.Now()
+		endTime = &defaultEndTime
+	}
 
-    // Debug prints
-    log.Printf("StartTime: %v, EndTime: %v", startTime, endTime)
+	log.Printf("StartTime: %v, EndTime: %v", startTime, endTime)
 
-    cloudwatchMetricData := map[string]float64{}
+	cloudwatchMetricData := make(map[string]*cloudwatch.GetMetricDataOutput)
 
-    // Fetch raw data
-    maxThrottleValue, err := GetMaxLambdaThrottleMetricValue(clientAuth, startTime, endTime, cloudWatchClient)
-    if err != nil {
-        log.Println("Error in getting maximum throttle value: ", err)
-        return "", nil, err
-    }
-    cloudwatchMetricData["MaxThrottles"] = maxThrottleValue
+	// Fetch raw data
+	rawData, err := GetLambdaThrottleMetricData(clientAuth, startTime, endTime, cloudWatchClient)
+	if err != nil {
+		return "", nil, err
+	}
+	cloudwatchMetricData["Throttling"] = rawData
 
-    // Debug prints
-    log.Printf("Maximum Throttle Value: %f", maxThrottleValue)
+	// Generate JSON response
+	jsonString, err := json.Marshal(rawData)
+	if err != nil {
+		return "", nil, err
+	}
 
-    jsonString, err := json.Marshal(ThrottleResult{Value: maxThrottleValue})
-    if err != nil {
-        log.Println("Error in marshalling json in string: ", err)
-        return "", nil, err
-    }
-
-    return string(jsonString), cloudwatchMetricData, nil
+	return string(jsonString), cloudwatchMetricData, err
 }
 
-func GetMaxLambdaThrottleMetricValue(clientAuth *model.Auth, startTime, endTime *time.Time, cloudWatchClient *cloudwatch.CloudWatch) (float64, error) {
-    input := &cloudwatch.GetMetricStatisticsInput{
-        Namespace:  aws.String("AWS/Lambda"),
-        MetricName: aws.String("Throttles"),
-        StartTime:  startTime,
-        EndTime:    endTime,
-        Period:     aws.Int64(2592000), 
-        Statistics: []*string{aws.String("Average")},
-    }
 
-    if cloudWatchClient == nil {
-        cloudWatchClient = awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH).(*cloudwatch.CloudWatch)
-    }
+func GetLambdaThrottleMetricData(clientAuth *model.Auth, startTime, endTime *time.Time, cloudWatchClient *cloudwatch.CloudWatch) (*cloudwatch.GetMetricDataOutput, error) {
+	input := &cloudwatch.GetMetricDataInput{
+		EndTime:   endTime,
+		StartTime: startTime,
+		MetricDataQueries: []*cloudwatch.MetricDataQuery{
+			{
+				Id: aws.String("throttle_query"),
+				MetricStat: &cloudwatch.MetricStat{
+					Metric: &cloudwatch.Metric{
+						MetricName: aws.String("Throttles"),
+						Namespace:  aws.String("AWS/Lambda"),
+					},
+					Period: aws.Int64(300), // 5 minutes
+					Stat:   aws.String("Average"),
+				},
+			},
+		},
+	}
 
-    result, err := cloudWatchClient.GetMetricStatistics(input)
-    if err != nil {
-        return 0, err
-    }
+	if cloudWatchClient == nil {
+		cloudWatchClient = awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH).(*cloudwatch.CloudWatch)
+	}
 
-    if len(result.Datapoints) == 0 {
-        return 0, fmt.Errorf("no data available for the specified time range")
-    }
+	result, err := cloudWatchClient.GetMetricData(input)
+	if err != nil {
+		return nil, err
+	}
 
-    // Extract the maximum value from the first datapoint
-    maxThrottleValue := aws.Float64Value(result.Datapoints[0].Maximum)
-
-    return maxThrottleValue, nil
+	return result, nil
 }
 
 func init() {
-    AwsxLambdaThrottleCmd.PersistentFlags().String("elementId", "", "element id")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("elementType", "", "element type")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("query", "", "query")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("cmdbApiUrl", "", "cmdb api")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("vaultUrl", "", "vault end point")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("vaultToken", "", "vault token")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("zone", "", "aws region")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("accessKey", "", "aws access key")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("secretKey", "", "aws secret key")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("crossAccountRoleArn", "", "aws cross account role arn")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("externalId", "", "aws external id")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("cloudWatchQueries", "", "aws cloudwatch metric queries")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("instanceId", "", "instance id")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("startTime", "", "start time")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("endTime", "", "end time")
-    AwsxLambdaThrottleCmd.PersistentFlags().String("responseType", "", "response type. json/frame")
+	AwsxLambdaThrottleCmd.PersistentFlags().String("startTime", "", "Start time")
+	AwsxLambdaThrottleCmd.PersistentFlags().String("endTime", "", "End time")
+	AwsxLambdaThrottleCmd.PersistentFlags().String("responseType", "", "Response type. json/frame")
 }
