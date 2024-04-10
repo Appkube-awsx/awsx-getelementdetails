a9686b87a7c419356827ec547e2c53a7b6f290f8
Mohd Mukarramuddin <mohd.mukarramuddin@synectiks.com>
2024-04-10 08:55:56 +0530

    NLB unhealthy host count panel

--- command/root.go
+++ command/root.go
@@ -2117,6 +2117,17 @@ var AwsxCloudWatchMetricsCmd = &cobra.Command{
 				} else {
 					fmt.Println(jsonResp)
 				}
+			} else if queryName == "unhealthy_host_count_panel" && (elementType == "AWS/NetworkELB" || elementType == "AWS/NLB") {
+				jsonResp, cloudwatchMetricResp, err := NLB.GetNLBUnhealthyHostCountPanel(cmd, clientAuth, nil)
+				if err != nil {
+					log.Println("Error getting read iops: ", err)
+					return
+				}
+				if responseType == "frame" {
+					fmt.Println(cloudwatchMetricResp)
+				} else {
+					fmt.Println(jsonResp)
+				}
 			} else {
 				fmt.Println("query not found")
 			}
@@ -2254,6 +2265,7 @@ func init() {
 	AwsxCloudWatchMetricsCmd.AddCommand(NLB.AwsxNLBNewConnectionsCmd)
 	AwsxCloudWatchMetricsCmd.AddCommand(NLB.AwsxNLBProcessedBytesCmd)
 	AwsxCloudWatchMetricsCmd.AddCommand(NLB.AwsxNLBHealthyHostCountCmd)
+	AwsxCloudWatchMetricsCmd.AddCommand(NLB.AwsxNLBUnhealthyHostCountCmd)
 
 	AwsxCloudWatchMetricsCmd.PersistentFlags().String("rootvolumeId", "", "root volume id")
 	AwsxCloudWatchMetricsCmd.PersistentFlags().String("ebsvolume1Id", "", "ebs volume 1 id")
---
+++ handler/NLB/unhealthy_host_count_panel.go
@@ -0,0 +1,174 @@
+package NLB
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Appkube-awsx/awsx-common/authenticate"
+	"github.com/Appkube-awsx/awsx-common/awsclient"
+	"github.com/Appkube-awsx/awsx-common/model"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/spf13/cobra"
+)
+
+type UnhealthyHostCountData struct {
+	UnhealthyHostCount []struct {
+		Timestamp time.Time
+		Value     float64
+	} `json:"UnhealthyHostCount"`
+}
+
+var AwsxNLBUnhealthyHostCountCmd = &cobra.Command{
+	Use:   "nlb_unhealthy_host_count_panel",
+	Short: "Get NLB unhealthy host count metrics data",
+	Long:  `Command to get NLB unhealthy host count metrics data`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Running from child command..")
+		var authFlag, clientAuth, err = authenticate.AuthenticateCommand(cmd)
+		if err != nil {
+			log.Printf("Error during authentication: %v\n", err)
+			err := cmd.Help()
+			if err != nil {
+				return
+			}
+			return
+		}
+		if authFlag {
+			responseType, _ := cmd.PersistentFlags().GetString("responseType")
+			jsonResp, cloudwatchMetricResp, err := GetNLBUnhealthyHostCountPanel(cmd, clientAuth, nil)
+			if err != nil {
+				log.Println("Error getting NLB unhealthy host count: ", err)
+				return
+			}
+			if responseType == "frame" {
+				fmt.Println(cloudwatchMetricResp)
+			} else {
+				// Default case. It prints JSON
+				fmt.Println(jsonResp)
+			}
+		}
+
+	},
+}
+
+func GetNLBUnhealthyHostCountPanel(cmd *cobra.Command, clientAuth *model.Auth, cloudWatchClient *cloudwatch.CloudWatch) (string, map[string]*cloudwatch.GetMetricDataOutput, error) {
+	nlbArn, _ := cmd.PersistentFlags().GetString("nlbArn")
+	startTimeStr, _ := cmd.PersistentFlags().GetString("startTime")
+	endTimeStr, _ := cmd.PersistentFlags().GetString("endTime")
+
+	var startTime, endTime *time.Time
+
+	if startTimeStr != "" {
+		parsedStartTime, err := time.Parse(time.RFC3339, startTimeStr)
+		if err != nil {
+			log.Printf("Error parsing start time: %v", err)
+			return "", nil, err
+		}
+		startTime = &parsedStartTime
+	} else {
+		defaultStartTime := time.Now().Add(-5 * time.Minute)
+		startTime = &defaultStartTime
+	}
+
+	if endTimeStr != "" {
+		parsedEndTime, err := time.Parse(time.RFC3339, endTimeStr)
+		if err != nil {
+			log.Printf("Error parsing end time: %v", err)
+			return "", nil, err
+		}
+		endTime = &parsedEndTime
+	} else {
+		defaultEndTime := time.Now()
+		endTime = &defaultEndTime
+	}
+
+	log.Printf("StartTime: %v, EndTime: %v", startTime, endTime)
+
+	cloudwatchMetricData := map[string]*cloudwatch.GetMetricDataOutput{}
+
+	// Fetch raw data
+	rawData, err := GetNLBUnhealthyHostCountMetricData(clientAuth, nlbArn, startTime, endTime, "Average", cloudWatchClient)
+	if err != nil {
+		log.Println("Error in getting NLB unhealthy host count data: ", err)
+		return "", nil, err
+	}
+	cloudwatchMetricData["UnhealthyHostCount"] = rawData
+
+	result := processNLBUnhealthyHostCountRawData(rawData)
+
+	jsonString, err := json.Marshal(result)
+	if err != nil {
+		log.Println("Error in marshalling json in string: ", err)
+		return "", nil, err
+	}
+
+	return string(jsonString), cloudwatchMetricData, nil
+}
+
+func GetNLBUnhealthyHostCountMetricData(clientAuth *model.Auth, nlbArn string, startTime, endTime *time.Time, statistic string, cloudWatchClient *cloudwatch.CloudWatch) (*cloudwatch.GetMetricDataOutput, error) {
+	log.Printf("Getting metric data for NLB %s from %v to %v", nlbArn, startTime, endTime)
+
+	input := &cloudwatch.GetMetricDataInput{
+		EndTime:   endTime,
+		StartTime: startTime,
+		MetricDataQueries: []*cloudwatch.MetricDataQuery{
+			{
+				Id: aws.String("m1"),
+				MetricStat: &cloudwatch.MetricStat{
+					Metric: &cloudwatch.Metric{
+						Dimensions: []*cloudwatch.Dimension{
+							{
+								Name:  aws.String("LoadBalancer"),
+								Value: aws.String("net/a0affec9643ca40c5a4e837eab2f07fb/f623f27b6210158f"),
+							},
+							{
+								Name:  aws.String("TargetGroup"),
+								Value: aws.String("targetgroup/k8s-istiosys-istioing-30129717de/b5e55c2955f8e65f"),
+							},
+						},
+						MetricName: aws.String("UnHealthyHostCount"),
+						Namespace:  aws.String("AWS/NetworkELB"),
+					},
+					Period: aws.Int64(60),
+					Stat:   aws.String(statistic),
+				},
+			},
+		},
+	}
+	if cloudWatchClient == nil {
+		cloudWatchClient = awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH).(*cloudwatch.CloudWatch)
+	}
+
+	result, err := cloudWatchClient.GetMetricData(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func processNLBUnhealthyHostCountRawData(result *cloudwatch.GetMetricDataOutput) UnhealthyHostCountData {
+	var rawData UnhealthyHostCountData
+	rawData.UnhealthyHostCount = make([]struct {
+		Timestamp time.Time
+		Value     float64
+	}, len(result.MetricDataResults[0].Timestamps))
+
+	for i, timestamp := range result.MetricDataResults[0].Timestamps {
+		rawData.UnhealthyHostCount[i].Timestamp = *timestamp
+		rawData.UnhealthyHostCount[i].Value = *result.MetricDataResults[0].Values[i]
+	}
+
+	return rawData
+}
+
+func init() {
+	AwsxNLBUnhealthyHostCountCmd.PersistentFlags().String("nlbArn", "", "NLB ARN")
+	AwsxNLBUnhealthyHostCountCmd.PersistentFlags().String("startTime", "", "start time")
+	AwsxNLBUnhealthyHostCountCmd.PersistentFlags().String("endTime", "", "end time")
+	AwsxNLBUnhealthyHostCountCmd.PersistentFlags().String("responseType", "", "response type. json/frame")
+}
