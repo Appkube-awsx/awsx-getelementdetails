d137a1646590c833ce1904ee61c5bdb47cfe63f0
kafreen <afreen.khan@synectiks.com>
2024-04-24 14:48:44 +0530

    created new function for NLB and Lambda in global file

--- global-function/metricData/comman_metric.go
+++ global-function/metricData/comman_metric.go
@@ -123,3 +123,79 @@ func GetMetricDatabaseData(clientAuth *model.Auth, instanceID, elementType strin
 
 	return result, nil
 }
+
+func GetMetricLoadBalancerData(clientAuth *model.Auth, instanceID, elementType string, metricName string, startTime, endTime *time.Time, statistic string, cloudWatchClient *cloudwatch.CloudWatch) (*cloudwatch.GetMetricDataOutput, error) {
+	log.Printf("Getting metric data for instance %s in namespace %s from %v to %v", instanceID, elementType, startTime, endTime)
+	input := &cloudwatch.GetMetricDataInput{
+		EndTime:   endTime,
+		StartTime: startTime,
+		MetricDataQueries: []*cloudwatch.MetricDataQuery{
+			{
+				Id: aws.String("m1"),
+				MetricStat: &cloudwatch.MetricStat{
+
+					Metric: &cloudwatch.Metric{
+						Dimensions: []*cloudwatch.Dimension{
+							{
+								Name:  aws.String("LoadBalancer"),
+								Value: aws.String(instanceID),
+							},
+						},
+						MetricName: aws.String(metricName),
+						Namespace:  aws.String(elementType),
+					},
+					Period: aws.Int64(300),
+					Stat:   aws.String(statistic),
+				},
+			},
+		},
+	}
+	if cloudWatchClient == nil {
+		cloudWatchClient = awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH).(*cloudwatch.CloudWatch)
+	}
+
+	result, err := cloudWatchClient.GetMetricData(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func GetMetricFunctionNameData(clientAuth *model.Auth, instanceID, elementType string, metricName string, startTime, endTime *time.Time, statistic string, cloudWatchClient *cloudwatch.CloudWatch) (*cloudwatch.GetMetricDataOutput, error) {
+	log.Printf("Getting metric data for instance %s in namespace %s from %v to %v", instanceID, elementType, startTime, endTime)
+	input := &cloudwatch.GetMetricDataInput{
+		EndTime:   endTime,
+		StartTime: startTime,
+		MetricDataQueries: []*cloudwatch.MetricDataQuery{
+			{
+				Id: aws.String("m1"),
+				MetricStat: &cloudwatch.MetricStat{
+
+					Metric: &cloudwatch.Metric{
+						Dimensions: []*cloudwatch.Dimension{
+							{
+								Name:  aws.String("FunctionName"),
+								Value: aws.String(instanceID),
+							},
+						},
+						MetricName: aws.String(metricName),
+						Namespace:  aws.String(elementType),
+					},
+					Period: aws.Int64(300),
+					Stat:   aws.String(statistic),
+				},
+			},
+		},
+	}
+	if cloudWatchClient == nil {
+		cloudWatchClient = awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH).(*cloudwatch.CloudWatch)
+	}
+
+	result, err := cloudWatchClient.GetMetricData(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
