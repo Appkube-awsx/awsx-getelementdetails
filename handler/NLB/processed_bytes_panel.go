1b2fee770a20459c259edf7d10c8a1537ffa443e
farahkhan01 <farah242khan@gmail.com>
2024-04-25 14:48:11 +0530

    refactor the processed bytes panel for NLB

--- handler/NLB/processed_bytes_panel.go
+++ handler/NLB/processed_bytes_panel.go
@@ -1,25 +1,16 @@
 package NLB
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/Appkube-awsx/awsx-common/authenticate"
-	"github.com/Appkube-awsx/awsx-common/awsclient"
 	"github.com/Appkube-awsx/awsx-common/model"
-	"github.com/aws/aws-sdk-go/aws"
+	"github.com/Appkube-awsx/awsx-getelementdetails/global-function/commanFunction"
+	"github.com/Appkube-awsx/awsx-getelementdetails/global-function/metricData"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/spf13/cobra"
 )
-
-type ProcessedBytesData struct {
-	ProcessedBytes []struct {
-		Timestamp time.Time
-		Value     float64
-	} `json:"ProcessedBytes"`
-}
 
 var AwsxNLBProcessedBytesCmd = &cobra.Command{
 	Use:   "nlb_processed_bytes_panel",
@@ -56,110 +47,31 @@ var AwsxNLBProcessedBytesCmd = &cobra.Command{
 }
 
 func GetNLBProcessedBytesPanel(cmd *cobra.Command, clientAuth *model.Auth, cloudWatchClient *cloudwatch.CloudWatch) (string, map[string]*cloudwatch.GetMetricDataOutput, error) {
-	nlbArn, _ := cmd.PersistentFlags().GetString("nlbArn")
-	startTimeStr, _ := cmd.PersistentFlags().GetString("startTime")
-	endTimeStr, _ := cmd.PersistentFlags().GetString("endTime")
+	elementType, _ := cmd.PersistentFlags().GetString("elementType")
+	fmt.Println(elementType)
+	instanceId, _ := cmd.PersistentFlags().GetString("instanceId")
 
-	var startTime, endTime *time.Time
-
-	if startTimeStr != "" {
-		parsedStartTime, err := time.Parse(time.RFC3339, startTimeStr)
-		if err != nil {
-			log.Printf("Error parsing start time: %v", err)
-			return "", nil, err
-		}
-		startTime = &parsedStartTime
-	} else {
-		defaultStartTime := time.Now().Add(-5 * time.Minute)
-		startTime = &defaultStartTime
+	startTime, endTime, err := commanFunction.ParseTimes(cmd)
+	if err != nil {
+		return "", nil, fmt.Errorf("error parsing time: %v", err)
 	}
 
-	if endTimeStr != "" {
-		parsedEndTime, err := time.Parse(time.RFC3339, endTimeStr)
-		if err != nil {
-			log.Printf("Error parsing end time: %v", err)
-			return "", nil, err
-		}
-		endTime = &parsedEndTime
-	} else {
-		defaultEndTime := time.Now()
-		endTime = &defaultEndTime
+	instanceId, err = commanFunction.GetCmdbData(cmd)
+	if err != nil {
+		return "", nil, fmt.Errorf("error getting instance ID: %v", err)
 	}
-
-	log.Printf("StartTime: %v, EndTime: %v", startTime, endTime)
 
 	cloudwatchMetricData := map[string]*cloudwatch.GetMetricDataOutput{}
 
 	// Fetch raw data
-	rawData, err := GetNLBProcessedBytesMetricData(clientAuth, nlbArn, startTime, endTime, "Sum", cloudWatchClient)
+	rawData, err := metricData.GetMetricLoadBalancerData(clientAuth, instanceId, "AWS/NetworkELB", "ProcessedBytes", startTime, endTime, "Sum", cloudWatchClient)
 	if err != nil {
 		log.Println("Error in getting NLB processed bytes data: ", err)
 		return "", nil, err
 	}
 	cloudwatchMetricData["ProcessedBytes"] = rawData
 
-	result := processNLBProcessedBytesRawData(rawData)
-
-	jsonString, err := json.Marshal(result)
-	if err != nil {
-		log.Println("Error in marshalling json in string: ", err)
-		return "", nil, err
-	}
-
-	return string(jsonString), cloudwatchMetricData, nil
-}
-
-func GetNLBProcessedBytesMetricData(clientAuth *model.Auth, nlbArn string, startTime, endTime *time.Time, statistic string, cloudWatchClient *cloudwatch.CloudWatch) (*cloudwatch.GetMetricDataOutput, error) {
-	log.Printf("Getting metric data for NLB %s from %v to %v", nlbArn, startTime, endTime)
-
-	input := &cloudwatch.GetMetricDataInput{
-		EndTime:   endTime,
-		StartTime: startTime,
-		MetricDataQueries: []*cloudwatch.MetricDataQuery{
-			{
-				Id: aws.String("m1"),
-				MetricStat: &cloudwatch.MetricStat{
-					Metric: &cloudwatch.Metric{
-						Dimensions: []*cloudwatch.Dimension{
-							{
-								Name:  aws.String("LoadBalancer"),
-								Value: aws.String("net/a0affec9643ca40c5a4e837eab2f07fb/f623f27b6210158f"),
-							},
-						},
-						MetricName: aws.String("ProcessedBytes"),
-						Namespace:  aws.String("AWS/NetworkELB"),
-					},
-					Period: aws.Int64(60),
-					Stat:   aws.String(statistic),
-				},
-			},
-		},
-	}
-	if cloudWatchClient == nil {
-		cloudWatchClient = awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH).(*cloudwatch.CloudWatch)
-	}
-
-	result, err := cloudWatchClient.GetMetricData(input)
-	if err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
-func processNLBProcessedBytesRawData(result *cloudwatch.GetMetricDataOutput) ProcessedBytesData {
-	var rawData ProcessedBytesData
-	rawData.ProcessedBytes = make([]struct {
-		Timestamp time.Time
-		Value     float64
-	}, len(result.MetricDataResults[0].Timestamps))
-
-	for i, timestamp := range result.MetricDataResults[0].Timestamps {
-		rawData.ProcessedBytes[i].Timestamp = *timestamp
-		rawData.ProcessedBytes[i].Value = *result.MetricDataResults[0].Values[i]
-	}
-
-	return rawData
+	return "", cloudwatchMetricData, nil
 }
 
 func init() {
