3e94c356a3484488b34385258162fcd35e92cb7e
farahkhan01 <farah242khan@gmail.com>
2024-04-30 16:34:58 +0530

    updated comman metric

--- global-function/commanFunction/comman_metric.go
+++ global-function/commanFunction/comman_metric.go
@@ -199,3 +199,40 @@ func GetMetricFunctionNameData(clientAuth *model.Auth, instanceID, elementType s
 
 	return result, nil
 }
+func GetMetricAPIData(clientAuth *model.Auth, instanceID, elementType string, metricName string, startTime, endTime *time.Time, statistic string, cloudWatchClient *cloudwatch.CloudWatch) (*cloudwatch.GetMetricDataOutput, error) {
+	log.Printf("Getting metric data for instance %s in namespace %s from %v to %v", instanceID, elementType, startTime, endTime)
+	input := &cloudwatch.GetMetricDataInput{
+		EndTime:   endTime,
+		StartTime: startTime,
+		MetricDataQueries: []*cloudwatch.MetricDataQuery{
+			{
+				Id: aws.String("m1"),
+				MetricStat: &cloudwatch.MetricStat{
+
+					Metric: &cloudwatch.Metric{
+						Dimensions: []*cloudwatch.Dimension{
+							{
+								Name:  aws.String("ApiName"),
+								Value: aws.String(instanceID),
+							},
+						},
+						MetricName: aws.String(metricName),
+						Namespace:  aws.String(elementType),
+					},
+					Period: aws.Int64(300),
+					Stat:   aws.String(statistic),
+				},
+			},
+		},
+	}
+	if cloudWatchClient == nil {
+		cloudWatchClient = awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH).(*cloudwatch.CloudWatch)
+	}
+
+	result, err := cloudWatchClient.GetMetricData(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
