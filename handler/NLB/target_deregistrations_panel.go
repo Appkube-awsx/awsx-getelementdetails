9976241aaae492e462cfe2f9b84a7a65a8797629
Ramya Pasupuleti <ramya.pasupuleti@synectiks.com>
2024-05-06 15:50:15 +0530

    refactor the target_deregistrations panel

--- handler/NLB/target_deregistrations_panel.go
+++ handler/NLB/target_deregistrations_panel.go
@@ -3,14 +3,10 @@ package NLB
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/Appkube-awsx/awsx-common/authenticate"
-	"github.com/Appkube-awsx/awsx-common/awsclient"
-	"github.com/Appkube-awsx/awsx-common/cmdb"
-	"github.com/Appkube-awsx/awsx-common/config"
 	"github.com/Appkube-awsx/awsx-common/model"
-	"github.com/aws/aws-sdk-go/aws"
+	 "github.com/Appkube-awsx/awsx-getelementdetails/comman-function"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/spf13/cobra"
 )
@@ -49,137 +45,31 @@ var AwsxTargetDeregistrationsCmd = &cobra.Command{
 
 func GetTargetDeregistrationspanel(cmd *cobra.Command, clientAuth *model.Auth, cloudWatchLogs *cloudwatchlogs.CloudWatchLogs) ([]*cloudwatchlogs.GetQueryResultsOutput, error) {
 	logGroupName, _ := cmd.PersistentFlags().GetString("logGroupName")
-	elementId, _ := cmd.PersistentFlags().GetString("elementId")
-	cmdbApiUrl, _ := cmd.PersistentFlags().GetString("cmdbApiUrl")
+	startTime, endTime, err := comman_function.ParseTimes(cmd)
 
-	if elementId != "" {
-		log.Println("getting cloud-element data from cmdb")
-		apiUrl := cmdbApiUrl
-		if cmdbApiUrl == "" {
-			log.Println("using default cmdb url")
-			apiUrl = config.CmdbUrl
-		}
-		log.Println("cmdb url: " + apiUrl)
-		cmdbData, err := cmdb.GetCloudElementData(apiUrl, elementId)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("Error parsing time: %v", err)
 		}
-		logGroupName = cmdbData.LogGroup
+		logGroupName, err = comman_function.GetCmdbLogsData(cmd)
 
-	}
-	startTimeStr, _ := cmd.PersistentFlags().GetString("startTime")
-	endTimeStr, _ := cmd.PersistentFlags().GetString("endTime")
-
-	var startTime, endTime *time.Time
-
-	// Parse start time if provided
-	if startTimeStr != "" {
-		parsedStartTime, err := time.Parse(time.RFC3339, startTimeStr)
+	
 		if err != nil {
-			log.Printf("Error parsing start time: %v", err)
-			err := cmd.Help()
-			if err != nil {
-
-			}
+			return nil, fmt.Errorf("error getting instance ID: %v", err)
 		}
-		startTime = &parsedStartTime
-	} else {
-		defaultStartTime := time.Now().Add(-5 * time.Minute)
-		startTime = &defaultStartTime
-	}
-
-	if endTimeStr != "" {
-		parsedEndTime, err := time.Parse(time.RFC3339, endTimeStr)
-		if err != nil {
-			log.Printf("Error parsing end time: %v", err)
-			err := cmd.Help()
-			if err != nil {
-				// handle error
-			}
-		}
-		endTime = &parsedEndTime
-	} else {
-		defaultEndTime := time.Now()
-		endTime = &defaultEndTime
-	}
-	results, err := FilterTargetDeregistration(clientAuth, startTime, endTime, logGroupName, cloudWatchLogs)
+		results, err := comman_function.GetLogsData(clientAuth, startTime, endTime, logGroupName, `fields @timestamp| filter eventSource=="elasticloadbalancing.amazonaws.com"| filter eventName=="DeregisterTargets"| stats count(*) as DeregistrationTargetCount by @timestamp| sort @timestamp desc`, cloudWatchLogs)
+		
 	if err != nil {
 		return nil, nil
 	}
-	processedResults := processQueryResult(results)
+	processedResults := comman_function.ProcessQueryResult(results)
 
 	return processedResults, nil
 }
 
-func FilterTargetDeregistration(clientAuth *model.Auth, startTime, endTime *time.Time, logGroupName string, cloudWatchLogs *cloudwatchlogs.CloudWatchLogs) ([]*cloudwatchlogs.GetQueryResultsOutput, error) {
-	params := &cloudwatchlogs.StartQueryInput{
-		LogGroupName: aws.String(logGroupName),
-		StartTime:    aws.Int64(startTime.Unix() * 1000),
-		EndTime:      aws.Int64(endTime.Unix() * 1000),
-		QueryString: aws.String(`fields @timestamp
-		| filter eventSource=="elasticloadbalancing.amazonaws.com"
-		| filter eventName=="DeregisterTargets"
-		| stats count(*) as DeregistrationTargetCount by @timestamp
-		| sort @timestamp desc`),
-	}
-	if cloudWatchLogs == nil {
-		cloudWatchLogs = awsclient.GetClient(*clientAuth, awsclient.CLOUDWATCH_LOG).(*cloudwatchlogs.CloudWatchLogs)
-	}
 
-	queryResult, err := cloudWatchLogs.StartQuery(params)
-	if err != nil {
-		return nil, fmt.Errorf("failed to start query: %v", err)
-	}
 
-	queryId := queryResult.QueryId
-	var queryResults []*cloudwatchlogs.GetQueryResultsOutput // Declare queryResults outside the loop
-	for {
-		// Check query status
-		queryStatusInput := &cloudwatchlogs.GetQueryResultsInput{
-			QueryId: queryId,
-		}
-
-		queryResult, err := cloudWatchLogs.GetQueryResults(queryStatusInput)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get query results: %v", err)
-		}
-
-		queryResults = append(queryResults, queryResult)
-
-		if *queryResult.Status != "Complete" {
-			time.Sleep(5 * time.Second) // wait before querying again
-			continue
-		}
-
-		break // exit loop if query is complete
-	}
-
-	return queryResults, nil
-}
-func processQueryResult(results []*cloudwatchlogs.GetQueryResultsOutput) []*cloudwatchlogs.GetQueryResultsOutput {
-	processedResults := make([]*cloudwatchlogs.GetQueryResultsOutput, 0)
-
-	for _, result := range results {
-		if *result.Status == "Complete" {
-			for _, resultField := range result.Results {
-				for _, data := range resultField {
-					if *data.Field == "failed" {
-
-						log.Printf("failed: %s\n", *data)
-
-						// You can perform further processing or store the instance count data as needed
-					}
-				}
-			}
-			processedResults = append(processedResults, result)
-
-		} else {
-			log.Println("Query status is not complete.")
-		}
-	}
-
-	return processedResults
-}
+	
+	
 
 func init() {
 	AwsxTargetDeregistrationsCmd.PersistentFlags().String("logGroupName", "", "log group name")
