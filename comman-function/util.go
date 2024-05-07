c9908a27389179111f91966f9eca618151484da5
mohit-sharma-synectiks <mohit.sharma@synectiks.com>
2024-05-07 16:59:01 +0530

    updated func init

--- comman-function/util.go
+++ comman-function/util.go
@@ -86,3 +86,29 @@ func GetCmdbLogsData(cmd *cobra.Command) (string, error) {
 
 	return "", errors.New("element ID is required")
 }
+
+func InitAwsCmdFlags(cmd *cobra.Command) {
+	cmd.PersistentFlags().String("rootvolumeId", "", "root volume id")
+	cmd.PersistentFlags().String("ebsvolume1Id", "", "ebs volume 1 id")
+	cmd.PersistentFlags().String("ebsvolume2Id", "", "ebs volume 2 id")
+	cmd.PersistentFlags().String("elementId", "", "element id")
+	cmd.PersistentFlags().String("cmdbApiUrl", "", "cmdb api")
+	cmd.PersistentFlags().String("vaultUrl", "", "vault end point")
+	cmd.PersistentFlags().String("vaultToken", "", "vault token")
+	cmd.PersistentFlags().String("accountId", "", "aws account number")
+	cmd.PersistentFlags().String("zone", "", "aws region")
+	cmd.PersistentFlags().String("accessKey", "", "aws access key")
+	cmd.PersistentFlags().String("secretKey", "", "aws secret key")
+	cmd.PersistentFlags().String("crossAccountRoleArn", "", "aws cross account role arn")
+	cmd.PersistentFlags().String("externalId", "", "aws external id")
+	cmd.PersistentFlags().String("cloudWatchQueries", "", "aws cloudwatch metric queries")
+	cmd.PersistentFlags().String("ServiceName", "", "Service Name")
+	cmd.PersistentFlags().String("elementType", "", "element type")
+	cmd.PersistentFlags().String("instanceId", "", "instance id")
+	cmd.PersistentFlags().String("clusterName", "", "cluster name")
+	cmd.PersistentFlags().String("query", "", "query")
+	cmd.PersistentFlags().String("startTime", "", "start time")
+	cmd.PersistentFlags().String("endTime", "", "endcl time")
+	cmd.PersistentFlags().String("responseType", "", "response type. json/frame")
+	cmd.PersistentFlags().String("logGroupName", "", "log group name")
+}
