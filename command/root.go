ca4e154caff05fc0fe10208fc51a7e40bb17a006
Chetan Meegada <chetan.meegada@synectiks.com>
2024-05-10 12:53:01 +0530

    invocation_panel root updated

--- command/root.go
+++ command/root.go
@@ -1403,17 +1403,17 @@ var AwsxCloudWatchMetricsCmd = &cobra.Command{
 				} else {
 					fmt.Println(jsonResp)
 				}
-			// } else if queryName == "error_breakdown_panel" && elementType == "Lambda" {
-			// 	jsonResp, err := Lambda.GetErrorBreakdownnewData(cmd, clientAuth, nil)
-			// 	if err != nil {
-			// 		log.Println("Error getting lambda failure  data: ", err)
-			// 		return
-			// 	}
-			// 	if responseType == "frame" {
-			// 		fmt.Println(jsonResp)
-			// 	} else {
-			// 		fmt.Println(jsonResp)
-			// 	}
+				// } else if queryName == "error_breakdown_panel" && elementType == "Lambda" {
+				// 	jsonResp, err := Lambda.GetErrorBreakdownnewData(cmd, clientAuth, nil)
+				// 	if err != nil {
+				// 		log.Println("Error getting lambda failure  data: ", err)
+				// 		return
+				// 	}
+				// 	if responseType == "frame" {
+				// 		fmt.Println(jsonResp)
+				// 	} else {
+				// 		fmt.Println(jsonResp)
+				// 	}
 			} else if queryName == "error_messages_count_panel" && elementType == "Lambda" {
 				jsonResp, err := Lambda.GetErrorMessageCountData(cmd, clientAuth, nil)
 				if err != nil {
@@ -1522,7 +1522,7 @@ var AwsxCloudWatchMetricsCmd = &cobra.Command{
 				} else {
 					fmt.Println(jsonResp)
 				}
-			} else if queryName == "invocation_panel" && elementType == "Lambda" {
+			} else if queryName == "invocation_panel" && elementType == "Lambda" || elementType == "LAMBDA" {
 				jsonResp, cloudwatchMetricResp, _ := Lambda.GetLambdaInvocationData(cmd, clientAuth, nil)
 				if err != nil {
 					log.Println("Error getting lambda error graph count data: ", err)
