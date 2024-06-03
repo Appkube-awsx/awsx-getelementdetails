10bbf70eccf95ec27da0ace5633928ebf77a9a70
NikithaBoredha <nikitha.boredha@synectiks.com>
2024-06-03 16:01:57 +0530

    added file execution_aborted_panel and execution_failed_panel

---
+++ handler/states/execution_failed_panel.go
@@ -0,0 +1,79 @@
+package States
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Appkube-awsx/awsx-common/authenticate"
+	"github.com/Appkube-awsx/awsx-common/model"
+	"github.com/Appkube-awsx/awsx-getelementdetails/comman-function"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/spf13/cobra"
+)
+
+var AwsxStatesExecutionFailedCmd = &cobra.Command{
+	Use:   "execution_failed_panel",
+	Short: "get execution failed metrics data",
+	Long:  `command to get execution failed metrics data`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("running from child command")
+		var authFlag, clientAuth, err = authenticate.AuthenticateCommand(cmd)
+		if err != nil {
+			log.Printf("Error during authentication: %v\n", err)
+			err := cmd.Help()
+			if err != nil {
+				return
+			}
+			return
+		}
+		if authFlag {
+			responseType, _ := cmd.PersistentFlags().GetString("responseType")
+			jsonResp, cloudwatchMetricResp, err := GetStepExecutionFailed(cmd, clientAuth, nil)
+			if err != nil {
+				log.Println("Error getting execution failed: ", err)
+				return
+			}
+			if responseType == "frame" {
+				fmt.Println(cloudwatchMetricResp)
+			} else {
+				// default case. it prints json
+				fmt.Println(jsonResp)
+			}
+		}
+
+	},
+}
+
+func GetStepExecutionFailed(cmd *cobra.Command, clientAuth *model.Auth, cloudWatchClient *cloudwatch.CloudWatch) (string, map[string]*cloudwatch.GetMetricDataOutput, error) {
+
+	elementType, _ := cmd.PersistentFlags().GetString("elementType")
+	fmt.Println(elementType)
+	instanceId, _ := cmd.PersistentFlags().GetString("instanceId")
+
+	startTime, endTime, err := comman_function.ParseTimes(cmd)
+	if err != nil {
+		return "", nil, fmt.Errorf("error parsing time: %v", err)
+	}
+
+	instanceId, err = comman_function.GetCmdbData(cmd)
+	if err != nil {
+		return "", nil, fmt.Errorf("error getting instance ID: %v", err)
+	}
+
+	cloudwatchMetricData := map[string]*cloudwatch.GetMetricDataOutput{}
+
+	// Fetch raw data
+	rawData, err := comman_function.GetMetricData(clientAuth, instanceId, "States", "ExecutionsFailed", startTime, endTime, "Sum", "StateMachineArn", cloudWatchClient)
+	if err != nil {
+		log.Println("Error in getting memory cache data: ", err)
+		return "", nil, err
+	}
+	cloudwatchMetricData["ExecutionsFailed"] = rawData
+
+	return "", cloudwatchMetricData, nil
+}
+
+func init() {
+	comman_function.InitAwsCmdFlags(AwsxStatesExecutionFailedCmd)
+}
